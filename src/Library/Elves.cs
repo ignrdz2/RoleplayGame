603a56bd7f85e4eec923ee4908a77d520eeab720
Gaston Bauer <gaston.bauer@cencosud.com.uy>
2023-09-14 19:28:01 -0300

    sacando codigo comentado

--- src/Library/Elves.cs
+++ src/Library/Elves.cs
@@ -5,38 +5,4 @@ public class Elfo : Personaje
     public Elfo(string name, int Initial_health, int Current_health) : base(name, "Elve", Initial_health, Current_health)
     {
     }
-
-    // public override int CalculateTotalAttack()
-    // {
-    //     int totalAttack = 0;
-    //     foreach (var item in Items)
-    //     {
-    //         totalAttack += item.Attack;
-    //     }
-    //     return totalAttack;
-    // }
-
-    // public override int CalculateTotalDefense()
-    // {
-    //     int totalDefense = 0;
-    //     foreach (var item in Items)
-    //     {
-    //         totalDefense += item.Defense;
-    //     }
-    //     return totalDefense;
-    // }
-
-    // public override void Attack(Personaje target, int damage)
-    // {
-    //     target.Current_health -= damage;
-    //     Console.WriteLine($"{target.Name} fue atacado con {damage} de damage, su vida ahora es de {target.Current_health}");
-    //     //Falta poder saber por quien fue atacado el target
-    // }
-
-    // public override void Heal(Personaje target)
-    // {
-    //     target.Current_health = target.Initial_health;
-    //     Console.WriteLine($"{target.Name} ha sido curado! | Vida: {target.Current_health}");
-    //     //Falta poder saber por quien fue curado el target
-    // }
 }
