91c18a51a8b030d3b1a9fb68e32ff132cb72cbdf
Axel Pedrozo <axel.pedrozo@correo.ucu.edu.uy>
2023-09-18 18:00:17 -0300

    actualizacion

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,25 +1,88 @@
 ﻿﻿using System;
+using System.Collections.Generic;
+
 
 namespace Program
 {
     class Program
     {
+        static void Play()
+        {
+            Console.WriteLine("Bienvenido a la batalla de los personajes");
+            System.Threading.Thread.Sleep(500);
+
+            Console.WriteLine("CREACION DE PERSONAJES");
+            var seguir = true;
+            Personaje personaje = null;
+            List<Personaje> personajes = new List<Personaje>();
+
+            while (seguir)
+            {
+                System.Threading.Thread.Sleep(500);
+                personaje = Game.CreateCharacter();
+
+                Console.WriteLine("Desea agregar item? (Y/N)");
+
+                var answer2 = Console.ReadLine();
+
+                if (answer2.ToLower().Trim() == "y")
+                {
+                    System.Threading.Thread.Sleep(500);
+                    Game.AddItem(personaje);
+                }
+
+                personajes.Add(personaje);
+
+                Console.WriteLine($"Se ha creado el personaje {personaje.Name} de tipo {personaje.Category}");
+
+                Console.WriteLine("Quiere crear otro personaje? (Y/N)");
+                var answer = Console.ReadLine();
+                if (answer.ToLower().Trim() == "n")
+                {
+                    seguir = false;
+                }
+
+            }
+
+            Console.Clear();
+            // Game.ShowCharacters(personajes);
+            System.Console.WriteLine("COMIENZA LA BATALLA");
+            System.Threading.Thread.Sleep(500);
+
+            Console.WriteLine("+-----------------------+");
+            Console.WriteLine("|+---------------------+|");
+            Console.WriteLine("||     PROXIMAMENTE    ||");
+            Console.WriteLine("|+---------------------+|");
+            Console.WriteLine("+-----------------------+");
+
+
+
+        }
+
         static void Main(string[] args)
         {
-            Wizard axeloncio = new Wizard("Axeloncio", 100, 100);
-            Wizard leoSalle = new Wizard("Panzas", 100, 100);
-
+            Wizard mago = new Wizard("Axel", 100, 100);
             SpellBook grimorio = new SpellBook();
-            axeloncio.SpecificItems[typeof(SpellBook)] = grimorio; //Agregar un grimorio para axeloncio
-            
-            Spell FireBall = new Spell("Bola de fuego", 23, 3, "fuego");
-            grimorio.AddSpell(FireBall);
-            axeloncio.UseSpell(FireBall, leoSalle, grimorio);
-            leoSalle.UseSpell(FireBall, axeloncio, grimorio);
+            mago.SpecificItems[typeof(SpellBook)] = grimorio;
 
-            Enano enanito = new Enano("Enanito", 150, 150);
-            enanito.SpecificItems[typeof(SpellBook)] = grimorio;
-            //Console.WriteLine($"{wizard.Name} | Vida: {wizard.Current_health} - Total Attack: {Wizard_totalAttack}, Total Defense: {Wizard_totalDefense}");
+            Enano victima = new Enano("Victima", 100, 100);
+
+            Spell hechizo = new Spell("Llamarada", 20, 5, "Fuego");
+            grimorio.AddSpell(hechizo);
+
+            mago.UseSpell(hechizo, victima, grimorio);
+
+            Wizard mago2 = new Wizard("Roberto", 100, 100);
+            SpellBook grimorio2 = new SpellBook();
+            mago2.SpecificItems[typeof(SpellBook)] = grimorio2;
+
+            // Intentar usar un hechizo que no está en el grimorio
+            mago2.UseSpell(hechizo, victima, grimorio2); /* No tiene hechizo en el grimorio */
+
+            // Intentar usar un grimorio ajeno
+            mago2.UseSpell(hechizo, victima, grimorio); /* No puede usar grimorio ajeno */
+
+            // Play();
         }
     }
 }
